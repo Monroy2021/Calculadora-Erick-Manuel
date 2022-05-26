e96cd3236d5678ca4b287ea3845e0f3d058ac8dd
Monroy2021 <luismonroy1014@gmail.com>
2022-05-25 21:43:39 -0500

    Descripcion

--- src/main/java/operaciones/aplicacionEjecutable.java
+++ src/main/java/operaciones/aplicacionEjecutable.java
@@ -6,21 +6,20 @@ import java.util.logging.Logger;
 public class aplicacionEjecutable {
 
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger("logg");
-        Scanner inp = new Scanner(System.in);
+        Logger logger = Logger.getLogger("logg"); /* Importamos la libreria logger para generar los mensajes en consola*/
+        Scanner leer = new Scanner(System.in);  /* Importamos la libreria Scanner */
 
-        logger.info("ingrese el primer y segundo numero:");
-        int num1, num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
+        logger.info("Ingrese el primer y segundo numero:"); /* Generamos el primer logger donde generamos el primer mensajes indicando el ingreso de dos valores */
+        int num1, num2; /* Generamos dos variables numericas */
+        num1 = leer.nextInt();
+        num2 = leer.nextInt(); /* Agregamos por entrada en consola las dos variables numericas */
         int respuestas;
         System.out.println("Ingrese su selección: 1 para suma, 2 para Resta 3 para multiplicación y 4 para división:");
         int escoger;
-        escoger = inp.nextInt();
+        escoger = leer.nextInt();
         switch (escoger) {
             case 1:
                 logger.info(suma(num1, num2));
-
                 break;
             case 2:
                 logger.info(resta(num1, num2));
